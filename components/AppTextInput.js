66a8b1182ec0295a9c43984e6c1d10eddd51c35f
Jaycob Duffel <jaycobduffel@Jaycobs-MacBook-Air.local>
2020-10-16 13:22:05 -0700

    Create Reusable Error Message, AppFormField, and AppTextInput for use with formik

--- components/AppTextInput.js
+++ components/AppTextInput.js
@@ -1,27 +1,20 @@
 import React from "react";
 import { StyleSheet, View, TextInput } from "react-native";
 
-import defaultStyles from "../config/styles";
-
-export default function AppTextInput({ icon, width = "100%", ...props }) {
+export default function AppTextInput({ width = "100%", ...props }) {
   return (
     <View style={[styles.container, { width }]}>
-      <TextInput 
-      placeholderTextColor={defaultStyles.colours.medium}
-      style={defaultStyles.text} {...props} />
+      <TextInput  {...props} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: defaultStyles.colours.light,
+    backgroundColor: "lightgrey",
     borderRadius: 25,
     flexDirection: "row",
     padding: 15,
     marginVertical: 10,
-  },
-  icon: {
-    marginRight: 10,
   },
 });
