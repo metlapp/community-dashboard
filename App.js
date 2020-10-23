b0bef5a8c201dfd2337007f883f51b616bf11db8
Dawson <DawsonDease07@gmail.com>
2020-10-23 09:34:07 -0700

    deleted unused imports in app.js

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, View } from "react-native";
 import LoginScreen from "./screens/LoginScreen";
-import AsyncStorage from "@react-native-community/async-storage";
 import AuthContext from "./auth/Context";
-import HomeScreen from "./screens/HomeScreen";
 import AccountScreen from "./screens/AccountScreen";
 import { Provider as PaperProvider } from "react-native-paper";
 import authStorage from "./auth/Storage";
