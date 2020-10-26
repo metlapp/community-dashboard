30fad88eafc58678489df1eb40609ff993e7bdb7
Jaycob Duffel <jaycobduffel@Jaycobs-MacBook-Air.local>
2020-10-26 12:45:35 -0700

    initial commit

---
+++ screens/ForgotPasswordScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+export default function ForgotPasswordScreen() {
+  return <SafeAreaView></SafeAreaView>;
+}
+
+const styles = StyleSheet.create({});
