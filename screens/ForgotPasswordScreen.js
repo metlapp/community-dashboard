392f23da697a175e3cceab09be5a15686da0ec87
Jaycob Duffel <jaycobduffel@gmail.com>
2020-10-26 14:24:42 -0700

    Setting up password fields

--- screens/ForgotPasswordScreen.js
+++ screens/ForgotPasswordScreen.js
@@ -1,12 +1,67 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { useContext, useState } from "react";
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import * as Yup from "yup";
 
-export default function ForgotPasswordScreen() {
+import AppButton from "../components/AppButton";
+import AppFormField from "../components/AppFormField";
+import runCrypto from "../auth/crypto-hashing";
+import defaultStyles from "../config/defaultStyles";
+import ErrorMessage from "../components/ErrorMessage";
+import Form from "../components/Form";
+import AuthContext from "../auth/Context";
+import SubmitButton from "../components/SubmitButton";
+
+const validationSchema = Yup.object().shape({
+  password: Yup.string().required().min(8).label("Password"),
+  confirmPassword: Yup.string().required().min(8).label("Confirm Password"),
+});
+
+export default function RegisterPassword() {
+  const [errorVisible, setErrorVisible] = useState(false);
+  // const { formData, setFormData, step, setStep } = useContext(FormContext)
+  const { user, setUser } = useContext(AuthContext);
   return (
-    <SafeAreaView>
-      <Text>Change password</Text>
+    <SafeAreaView style={styles.container}>
+      <Form
+        initialValues={{ password: "", confirmPassword: "" }}
+        onSubmit={async (values) => {
+          if (values.password !== values.confirmPassword) {
+            setErrorVisible(true);
+            return;
+          }
+          const password = await runCrypto(values.password);
+          setUser(() => ({
+            ...user,
+            password: password,
+          }));
+          setErrorVisible(false);
+          setStep(step + 1);
+        }}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          name="password"
+          placeholder="New password"
+          secureTextEntry
+          style={defaultStyles.TextInput}
+          testID="pass"
+        />
+        <AppFormField
+          name="confirmPassword"
+          placeholder="Confirm password"
+          secureTextEntry
+          style={defaultStyles.TextInput}
+          testID="confirmPass"
+        />
+        <ErrorMessage error="Passwords do not match" visible={errorVisible} />
+        <SubmitButton title="Save" />
+      </Form>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+  },
+});
