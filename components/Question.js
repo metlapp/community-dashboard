bdfd11599b7c8cc1096b1e13f6feb4afc824b299
Dawson <DawsonDease07@gmail.com>
2020-11-18 11:24:02 -0800

    Almost finished

--- components/Question.js
+++ components/Question.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, TouchableOpacity, Image, StyleSheet, View } from "react-native";
 import { Surface, TextInput, Button } from "react-native-paper";
+import Slider from "@react-native-community/slider";
 
 const Question = (props) => {
+  const [answer, setAnswer] = React.useState("");
+  const [sliderValue, setSliderValue] = React.useState(1);
+
+  //Renders the right type of componets depending on question type
   switch (props.question.question_type) {
     case "YES_NO":
       return (
         <Surface>
           <Text style={styles.questionTitle}>{props.question.title}</Text>
           <View style={styles.container}>
-            <TouchableOpacity onPress={() => console.log("Pressed")}>
+            <TouchableOpacity onPress={() => props.answerCallBack(true)}>
               <Image
                 style={styles.image}
                 source={require("../images/check.png")}
               />
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => console.log("Pressed")}>
+            <TouchableOpacity onPress={() => props.answerCallBack(false)}>
               <Image
                 style={styles.image}
                 source={require("../images/x_mark.png")}
@@ -30,32 +35,19 @@ const Question = (props) => {
         <Surface>
           <Text style={styles.questionTitle}>{props.question.title}</Text>
           <View style={styles.container}>
-            <TouchableOpacity onPress={() => console.log("Pressed")}>
+            <TouchableOpacity onPress={() => props.answerCallBack(true)}>
               <Image
                 style={styles.image}
                 source={require("../images/happy.png")}
               />
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => console.log("Pressed")}>
+            <TouchableOpacity onPress={() => props.answerCallBack(false)}>
               <Image
                 style={styles.image}
                 source={require("../images/sad.png")}
               />
             </TouchableOpacity>
           </View>
-        </Surface>
-      );
-
-    case "ONE_SENTENCE":
-      return (
-        <Surface>
-          <Text style={styles.questionTitle}>{props.question.title}</Text>
-          <TextInput
-            style={styles.textInput}
-            placeholder="Answer"
-            multiline={true}
-          ></TextInput>
-          <Button mode="contained">Submit</Button>
         </Surface>
       );
 
@@ -67,27 +59,80 @@ const Question = (props) => {
             style={styles.textInput}
             placeholder="Answer"
             multiline={true}
+            onChangeText={(value) => {
+              setAnswer(value);
+            }}
           ></TextInput>
-          <Button mode="contained">Submit</Button>
+
+          <Button
+            mode="contained"
+            onPress={() => {
+              props.answerCallBack(answer);
+            }}
+          >
+            Submit
+          </Button>
         </Surface>
       );
 
     case "MULTILINE":
+      let rows = [];
+      let input = [];
+
+      for (let i = 0; i < props.question.number_of_lines; i++) {
+        rows.push(
+          <TextInput
+            key={i}
+            style={styles.textInput}
+            placeholder="Answer"
+            onChangeText={(value) => {
+              input[i] = value;
+            }}
+          ></TextInput>
+        );
+      }
+
       return (
         <Surface>
           <Text style={styles.questionTitle}>{props.question.title}</Text>
-          <View style={styles.container}>
-            <TextInput
-              style={styles.textInput}
-              placeholder="Answer"
-              multiline={true}
-            ></TextInput>
-          </View>
+          {rows}
+          <Button
+            mode="contained"
+            onPress={() => {
+              input = input.join(" || ");
+              props.answerCallBack(input);
+            }}
+          >
+            Submit
+          </Button>
         </Surface>
       );
 
     case "SCALE":
-      return <Text>S</Text>;
+      return (
+        <Surface>
+          <Text style={styles.questionTitle}>{props.question.title}</Text>
+          <Text style={styles.sliderValue}>{sliderValue}</Text>
+          <Slider
+            style={{ width: "100%", height: 40 }}
+            minimumValue={1}
+            maximumValue={5}
+            step={1}
+            thumbTintColor="purple"
+            onValueChange={(value) => {
+              setSliderValue(value);
+            }}
+          />
+          <Button
+            mode="contained"
+            onPress={() => {
+              props.answerCallBack(sliderValue);
+            }}
+          >
+            Submit
+          </Button>
+        </Surface>
+      );
   }
 };
 
@@ -107,7 +152,13 @@ const styles = StyleSheet.create({
   },
   textInput: {
     width: "90%",
-    margin: 20,
+    margin: 15,
+  },
+  sliderValue: {
+    textAlign: "center",
+    fontSize: 25,
+    fontWeight: "bold",
+    marginTop: 10,
   },
 });
 
--- screens/QuestionScreen.js
+++ screens/QuestionScreen.js
@@ -1,8 +1,5 @@
-import React, { useContext, useEffect } from "react";
-import AccountModal from "../components/AccountModal";
+import React, { useEffect } from "react";
 import { Appbar, Button } from "react-native-paper";
-import AuthContext from "../auth/Context";
-import authStorage from "../auth/Storage";
 import { SafeAreaView, StyleSheet, View, Text } from "react-native";
 import { apiConfig } from "../config/config";
 import axios from "axios";
@@ -12,10 +9,48 @@ const QuestionScreen = () => {
   const [loading, setLoading] = React.useState(true);
   const [data, setData] = React.useState();
 
+  const postData = async (payLoad) => {
+    axios
+      .post(apiConfig.baseUrl + "answers/", payLoad, {
+        auth: apiConfig.auth,
+      })
+      .then(() => {})
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  //Recieves Answers from child component and send it to the postData function to save
+  const AnswerCallBack = (answer) => {
+    //will need to change these values later to fit specific users and questions
+    let payLoad = { user: 1, question: 1 };
+
+    //deteermines what data type was answered
+    switch (data.question_type) {
+      case "YES_NO":
+        payLoad = { ...payLoad, bool_answer: answer };
+        break;
+      case "HAPPY_SAD":
+        payLoad = { ...payLoad, bool_answer: answer };
+        break;
+      case "BLOCK":
+        payLoad = { ...payLoad, string_answer: answer };
+        break;
+      case "MULTILINE":
+        payLoad = { ...payLoad, string_answer: answer };
+        break;
+      case "SCALE":
+        payLoad = { ...payLoad, number_answer: answer };
+        break;
+    }
+    postData(payLoad);
+  };
+
   useEffect(() => {
+    //Grabs the specific question from api
     const fetchData = async () => {
       axios
-        .get(apiConfig.baseUrl + "questions/9", {
+        .get(apiConfig.baseUrl + "questions/1", {
+          //This is hard coded until we get a solution later
           auth: apiConfig.auth,
         })
         .then((data) => {
@@ -33,7 +68,11 @@ const QuestionScreen = () => {
       <Appbar.Header>
         <Appbar.Content title="QUESTION" />
       </Appbar.Header>
-      {loading ? <Text>Loading</Text> : <Question question={data} />}
+      {loading ? (
+        <Text>Loading</Text>
+      ) : (
+        <Question question={data} answerCallBack={AnswerCallBack} />
+      )}
     </SafeAreaView>
   );
 };
