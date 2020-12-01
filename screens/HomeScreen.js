e834749a99562d49ae4d138c3285d8a6241314b7
Dawson Dease <dawsondease@Dawsons-Mac-mini.local>
2020-12-01 11:42:50 -0800

    answers now disapear when answered

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {
   SafeAreaView,
-  Text,
   Linking,
   StyleSheet,
   View,
@@ -9,98 +8,9 @@ import {
 } from "react-native";
 import { WebView } from "react-native-webview";
 import Question from "../components/Question";
-import { Appbar, Card, Title, Paragraph, Button } from "react-native-paper";
+import { Card, Title, Paragraph, Button } from "react-native-paper";
 import { apiConfig } from "../config/config";
 import axios from "axios";
-
-const postData = async (payLoad) => {
-  axios
-    .post(apiConfig.baseUrl + "answers/", payLoad, {
-      auth: apiConfig.auth,
-    })
-    .then(() => {})
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
-const DetermineContent = (props) => {
-  const answerCallBack = (answer) => {
-    //will need to change these values later to fit specific users and questions
-    let payLoad = { user: 1, question: props.content.content.item_object.id };
-
-    //detrmines what data type was answered
-    switch (props.content.content.item_object.question_type) {
-      case "YES_NO":
-        payLoad = { ...payLoad, bool_answer: answer };
-        break;
-      case "HAPPY_SAD":
-        payLoad = { ...payLoad, bool_answer: answer };
-        break;
-      case "BLOCK":
-        payLoad = { ...payLoad, string_answer: answer };
-        break;
-      case "MULTILINE":
-        payLoad = { ...payLoad, string_answer: answer };
-        break;
-      case "SCALE":
-        payLoad = { ...payLoad, number_answer: answer };
-        break;
-    }
-    postData(payLoad);
-  };
-
-  if (props.content.content.item_type == "Question") {
-    return (
-      <Question
-        question={props.content.content.item_object}
-        answerCallBack={answerCallBack}
-      />
-    );
-  }
-  switch (props.content.content.item_object.content_type) {
-    case "Video": {
-      return (
-        <View style={styles.container}>
-          <Title>{props.content.content.item_object.title}</Title>
-          <WebView
-            javaScriptEnabled={true}
-            scrollEnabled={false}
-            allowsFullscreenVideo={true}
-            source={{
-              uri: props.content.content.item_object.link,
-            }}
-          />
-        </View>
-      );
-    }
-    case "Article": {
-      return (
-        <Button
-          mode="outlined"
-          onPress={() => {
-            Linking.openURL(props.content.content.item_object.link);
-          }}
-        >
-          GO TO ARTICLE
-        </Button>
-      );
-    }
-  }
-};
-
-const renderFeed = ({ item }) => {
-  return (
-    <View style={styles.card}>
-      <Card style={styles.c}>
-        <Card.Content>
-          <DetermineContent content={item} />
-          <Paragraph>{item.content.item_object.description}</Paragraph>
-        </Card.Content>
-      </Card>
-    </View>
-  );
-};
 
 export default function HomeScreen() {
   const [content, setContent] = useState([]);
@@ -129,11 +39,110 @@ export default function HomeScreen() {
     fetchData();
   }, [page]);
 
+  const deleteItem = (id) => {
+    console.log(id);
+    let filteredData = content.filter((item) => item.id !== id);
+    console.log(filteredData);
+    setContent(filteredData);
+  };
+
+  //Determines the content to be displayed to the homescreen
+  const DetermineContent = (props) => {
+    //Posts the answer to the API
+    const postData = async (payLoad) => {
+      axios
+        .post(apiConfig.baseUrl + "answers/", payLoad, {
+          auth: apiConfig.auth,
+        })
+        .then(() => {
+          deleteItem(props.content.id);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+
+    //Determines what type the answer is ans stes up the payload to send to the API
+    const answerCallBack = (answer) => {
+      //will need to change these values later to fit specific users and questions
+      let payLoad = { user: 1, question: props.content.content.item_object.id };
+
+      //detrmines what data type was answered
+      switch (props.content.content.item_object.question_type) {
+        case "YES_NO":
+          payLoad = { ...payLoad, bool_answer: answer };
+          break;
+        case "HAPPY_SAD":
+          payLoad = { ...payLoad, bool_answer: answer };
+          break;
+        case "BLOCK":
+          payLoad = { ...payLoad, string_answer: answer };
+          break;
+        case "MULTILINE":
+          payLoad = { ...payLoad, string_answer: answer };
+          break;
+        case "SCALE":
+          payLoad = { ...payLoad, number_answer: answer };
+          break;
+      }
+      postData(payLoad);
+    };
+
+    if (props.content.content.item_type == "Question") {
+      return (
+        <Question
+          question={props.content.content.item_object}
+          answerCallBack={answerCallBack}
+        />
+      );
+    }
+    switch (props.content.content.item_object.content_type) {
+      case "Video": {
+        return (
+          <View style={styles.container}>
+            <Title>{props.content.content.item_object.title}</Title>
+            <WebView
+              javaScriptEnabled={true}
+              scrollEnabled={false}
+              allowsFullscreenVideo={true}
+              source={{
+                uri: props.content.content.item_object.link,
+              }}
+            />
+          </View>
+        );
+      }
+      case "Article": {
+        return (
+          <Button
+            mode="outlined"
+            onPress={() => {
+              Linking.openURL(props.content.content.item_object.link);
+            }}
+          >
+            GO TO ARTICLE
+          </Button>
+        );
+      }
+    }
+  };
+
+  //Renders the data in a pretty way to the flatlist
+  const renderFeed = ({ item }) => {
+    return (
+      <View style={styles.card}>
+        <Card style={styles.c}>
+          <Card.Content>
+            <DetermineContent content={item} />
+            <Paragraph>{item.content.item_object.description}</Paragraph>
+          </Card.Content>
+        </Card>
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView>
-      <Appbar.Header>
-        <Appbar.Content title="Home" />
-      </Appbar.Header>
       <View>
         <FlatList
           data={content}
@@ -154,9 +163,7 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   footer: {
-    flexDirection: "row",
     justifyContent: "center",
-    marginBottom: 150,
     marginTop: 6,
   },
   container: {
